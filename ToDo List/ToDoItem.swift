e4d4c0f654b678e4b81da34d88eae431400c5184
notMahad <abdima@bc.edu>
2022-02-21 15:40:17 -0500

    committing

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Mahad Abdi on 2/21/22.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
